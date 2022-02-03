0ca617446e1e3c425bdeece77dd5853a157cea6b
sindhuj1042 <96987474+sindhuj1042@users.noreply.github.com>
2022-02-03 20:46:55 +0530

    Update and rename area of circle  .py to area of circle.py

--- area of circle  .py
+++
@@ -1,4 +0,0 @@
-# area-of-circle
-r=1.1
-a="area of circle"
-print (a,3.14*r**2) 
---
+++ area of circle.py
@@ -0,0 +1,10 @@
+# area-of-circle
+r=1.1
+a="area of circle"
+print (a,3.14*r**2) 
+ 
+
+#file extension
+File=input("enter number") 
+Extension_file.split(".") 
+Print("extension of file", str(extension [-1]) 
